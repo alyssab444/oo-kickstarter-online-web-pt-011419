4642547133086324666a723f4dffc81bb35fe0e8
Alyssa Brown <alyssa.brown@live.com>
2019-02-15 00:35:20 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,11 @@
 class Backer 
+  attr_accessor :backed_projects
   attr_reader :name 
-  def initialize(name)
+  
+  def initialize(name, backed_projects)
     @name=name
+    @backed_projects= []
+    
   end 
   
 end 
